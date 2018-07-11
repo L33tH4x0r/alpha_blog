39cbc6b161b6a29399edad2a6569627efe2af294
austin <austin@dringo.io>
2018-07-11 15:04:07 -0700

    Completed Article in React with timestamp

---
+++ app/javascript/components/Article.js
@@ -0,0 +1,38 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import Timestamp from "react-timestamp"
+
+class Article extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div className="article-title">
+          <a href={this.props.path}>{this.props.title}</a>
+        </div>
+        <div className="article-body">
+          {this.props.description}
+        </div>
+        <div className="article-meta-details">
+          <small>
+            Created by: {this.props.author},
+            &nbsp;
+            <Timestamp time={this.props.created_at} precision={3} />
+            &nbsp;
+            Last Updated: <Timestamp time={this.props.updated_at} precision={3} />,
+          </small>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+Article.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  path: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string
+};
+export default Article
